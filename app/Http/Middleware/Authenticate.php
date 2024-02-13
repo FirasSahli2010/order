276ad3ac359133e92fe1d0392705df4118f9a6fe
FirasSahli2010 <feras.f.sahli@gmail.com>
2024-02-13 13:20:51 +0100

    Authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,7 +3,12 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
 use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use App\Http\Controllers\LoginController;
 
 class Authenticate extends Middleware
 {
@@ -12,6 +17,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        $loginController = new LoginController();
+        return $loginController->login($request);
     }
 }
